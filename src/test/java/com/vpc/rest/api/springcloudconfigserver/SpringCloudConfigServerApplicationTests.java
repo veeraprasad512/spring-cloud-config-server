9d8e2a78afac7dd284d07e6167c0a0d03fdae5d5
Veera Prasad Chirukuri <veeraprasad512@gmail.com>
2021-07-11 16:18:10 -0400

    First Commit

---
+++ src/test/java/com/vpc/rest/api/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpc.rest.api.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
